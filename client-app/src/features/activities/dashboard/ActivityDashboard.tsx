6f27804e3f8b076a0c1c557122e951f1a6ca5169
Ramiz Ismaili <rismaili49@gmail.com>
2022-03-29 00:09:19 +0200

    end of section 8

---
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -0,0 +1,28 @@
+import { observer } from 'mobx-react-lite';
+import React, { useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
+import { useStore } from '../../../app/stores/store';
+import ActivityList from './ActivityList';
+
+export default observer(function ActivityDashboard() {
+    const {activityStore} = useStore();
+    const {loadActivities, activityRegistry} = activityStore;
+
+    useEffect(() => {
+      if (activityRegistry.size <= 1) loadActivities();
+    }, [activityRegistry.size, loadActivities])
+  
+    if (activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
+
+    return (
+        <Grid>
+            <Grid.Column width='10'>
+                <ActivityList />
+            </Grid.Column>
+            <Grid.Column width='6'>
+                <h2>Activity filters</h2>
+            </Grid.Column>
+        </Grid>
+    )
+})
