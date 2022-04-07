3e1026a7caac52d09a7687889d7fbfc4b4db6053
Ramiz Ismaili <rismaili49@gmail.com>
2022-04-07 02:30:03 +0200

    End of section 11

---
+++ client-app/src/app/common/options/categoryOptions.ts
@@ -0,0 +1,8 @@
+export const categoryOptions = [
+    {text: 'Drinks', value: 'drinks'},
+    {text: 'Culture', value: 'culture'},
+    {text: 'Film', value: 'food'},
+    {text: 'Food', value: 'fod'},
+    {text: 'Music', value: 'music'},
+    {text: 'Travel', value: 'travel'},
+]
